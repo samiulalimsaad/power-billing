ad3629fa75cd452f9b9dce2c1ac80d214683b6a7
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-29 17:31:28 +0600

    bill rtk api added

---
+++ src/redux/features/bill/billApi.ts
@@ -0,0 +1,75 @@
+import toast from "react-hot-toast";
+import { apiSlice } from "../apiSlice";
+
+export const billsApi = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getBills: builder.query({
+            query: () => `/billing-list`,
+        }),
+        addBill: builder.mutation({
+            query: (data) => ({
+                url: "/add-billing",
+                method: "POST",
+                body: data,
+            }),
+        }),
+        editBill: builder.mutation({
+            query: ({ id, data }) => ({
+                url: `/update-billing/${id}`,
+                method: "PATCH",
+                body: data,
+            }),
+            onQueryStarted: async (
+                arg,
+                { queryFulfilled, dispatch, getState }
+            ) => {
+                try {
+                    const { data } = await queryFulfilled;
+
+                    if (data?._id) {
+                        toast.success("Profile updated successfully!");
+                    }
+                } catch (error) {
+                    toast.error((error as Error)?.message);
+                }
+            },
+        }),
+        deleteBill: builder.mutation({
+            query: (id) => ({
+                url: `/delete-billing/${id}`,
+                method: "DELETE",
+            }),
+            onQueryStarted: async (
+                arg,
+                { queryFulfilled, dispatch, getState }
+            ) => {
+                try {
+                    const { data } = await queryFulfilled;
+
+                    if (data?._id) {
+                        dispatch(
+                            apiSlice.util.updateQueryData(
+                                /* @ts-ignore:disable-next-line */
+                                "getBills",
+                                undefined,
+                                (draft) =>
+                                    draft.filter((v) => v._id !== data._id)
+                            )
+                        );
+
+                        toast.success("Bill deleted successfully!");
+                    }
+                } catch (error) {
+                    toast.error((error as Error)?.message);
+                }
+            },
+        }),
+    }),
+});
+
+export const {
+    useGetBillsQuery,
+    useAddBillMutation,
+    useEditBillMutation,
+    useDeleteBillMutation,
+} = billsApi;
