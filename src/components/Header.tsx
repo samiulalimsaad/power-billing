ac96fb82afa428e192a4bcacd914983ac0414f47
samiulalimsaad <samiulalimsaad@gmail.com>
2023-01-29 11:54:41 +0600

    header added

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div className="navbar bg-slate-800 text-white">
+            <div className="flex-1">
+                <a className="btn btn-ghost normal-case text-xl">Logo</a>
+            </div>
+            <div className="flex-none">Paid Total:0</div>
+        </div>
+    );
+};
+
+export default Header;
